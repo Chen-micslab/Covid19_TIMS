f7fb5692ec3ccc779db35b1954f2917bdc67141c
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2021-02-26 19:20:44 +0800

    Rename differential correlation.R to differential_correlation.R

--- R/differential_correlation.R
+++ R/differential_correlation.R
@@ -8,7 +8,7 @@ cell_type = c(rep('case',n_case),rep('control',n_control))
 design_mat1 = makeDesign(cell_type)
 
 
-#使用spearman相关系数，生成包含differential correlation的dataframe
+#浣跨敤spearman鐩稿叧绯绘暟锛岀敓鎴愬寘鍚玠ifferential correlation鐨刣ataframe
 ddcor_res1 = ddcorAll(inputMat = data1, design = design_mat1,compare = c("case", "control"),
                      adjust = "BH", heatmapPlot = FALSE,nPerms = 0,corrType = "spearman", nPairs = 'all')
 write.csv(ddcor_res1,'./data/differential corr.CSV')
