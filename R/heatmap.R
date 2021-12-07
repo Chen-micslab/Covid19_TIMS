dea8539feb985ae04b08d8e856dad5c6656bc5db
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2021-12-07 16:23:43 +0800

    Update heatmap.R

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,5 +1,5 @@
 library(pheatmap)
-data <- read.csv('./data/area matrix.CSV',row.names = 1,header = T,stringsAsFactors = F)
+data = read.csv('./data/area matrix.CSV',row.names = 1,header = T,stringsAsFactors = F)
 data1 = data
 data1 = log10(data1)
 for (i in 1:length(row.names(data1))){
@@ -7,18 +7,13 @@ for (i in 1:length(row.names(data1))){
   b = scale(a,center = TRUE,scale = TRUE)
   data1[i,] = b
 }
-
 z = read.csv('./data/annotation.CSV',header = T,stringsAsFactors = F)
 x=colnames(data) 
 rownames(z)<-x
-
-
-bk <- c(seq(-9,-0.1,by=0.02),seq(0,9,by=0.02))
-
+bk = c(seq(-9,-0.1,by=0.02),seq(0,9,by=0.02))
 ac = list(
   Type = c(Asymptomatic = "#990000", Healthy = "#663399"),
   Sex = c(Male = "#cc9900", Female = "#336600")
-
 )
 pheatmap(data1,scale = 'none',annotation_col=z,cluster_cols = FALSE,show_colnames = F,show_rownames = T,cluster_rows = FALSE,fontsize_row = 10, 
          color = c(colorRampPalette(colors = c("blue","white"))(length(bk)/2),colorRampPalette(colors = c("white","red"))(length(bk)/2)),
