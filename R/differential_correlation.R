bf82e7d36b989c234663217199f5d0a62db076c0
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2021-12-07 16:46:24 +0800

    Update differential_correlation.R

--- R/differential_correlation.R
+++ R/differential_correlation.R
@@ -1,5 +1,5 @@
 library(DGCA)
-data = read.csv("./data/432 lipid.csv",header = T,row.names = 1)
+data = read.csv("./data/432_lipid.csv",header = T,row.names = 1)
 data1 = data
 n_case = 89  ### n_case is the number of case
 n_control = 178  ### n_control is the number of control
@@ -7,7 +7,7 @@ cell_type = c(rep('case',n_case),rep('control',n_control))
 design_mat1 = makeDesign(cell_type)
 ddcor_res1 = ddcorAll(inputMat = data1, design = design_mat1,compare = c("case", "control"),
                      adjust = "BH", heatmapPlot = FALSE,nPerms = 0,corrType = "spearman", nPairs = 'all')
-write.csv(ddcor_res1,'./data/differential corr.csv')
+write.csv(ddcor_res1,'./data/differential_corr.csv')
 
 
 
