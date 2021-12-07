858f41e741e4bf7f7a2124ec213ee8edc416aa0b
Chen-micslab <79675347+Chen-micslab@users.noreply.github.com>
2021-12-07 16:21:10 +0800

    Update differential_correlation.R

--- R/differential_correlation.R
+++ R/differential_correlation.R
@@ -1,17 +1,13 @@
 library(DGCA)
 data = read.csv("./data/432 lipid.csv",header = T,row.names = 1)
 data1 = data
-
-n_case = 89
-n_control = 178
+n_case = 89  ### n_case is the number of case
+n_control = 178  ### n_control is the number of control
 cell_type = c(rep('case',n_case),rep('control',n_control))
 design_mat1 = makeDesign(cell_type)
-
-
-#使用spearman相关系数，生成包含differential correlation的dataframe
 ddcor_res1 = ddcorAll(inputMat = data1, design = design_mat1,compare = c("case", "control"),
                      adjust = "BH", heatmapPlot = FALSE,nPerms = 0,corrType = "spearman", nPairs = 'all')
-write.csv(ddcor_res1,'./data/differential corr.CSV')
+write.csv(ddcor_res1,'./data/differential corr.csv')
 
 
 
